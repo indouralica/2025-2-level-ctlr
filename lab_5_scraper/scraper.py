465b5596a25dcde2238cf57fd1edf851fbfe13be
indouralica <nordwest470@yandex.ru>
2026-05-18 00:50:26 +0300

    added new line in scraper (Newline check test) and - i guess - sorted imports (Import Style checks)

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -8,12 +8,12 @@ import re
 import shutil
 from typing import Union
 
+from bs4 import BeautifulSoup
 import requests
 from requests.exceptions import RequestException
-from bs4 import BeautifulSoup
 
 from core_utils.article.article import Article
-from core_utils.article.io import to_raw, to_meta
+from core_utils.article.io import to_meta, to_raw
 from core_utils.config_dto import ConfigDTO
 from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 
